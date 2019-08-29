16149f7cda9d4249b280816fb190299b5356863b
Valera <Valerakot97@gmail.com>
2019-08-29 16:46:15 +0300

    First commit

--- src/views/test/index.php
+++ src/views/test/index.php
@@ -1,9 +1,9 @@
 <?php
-
+use yii\helpers\Url;
 ?>
 <a href="<?=Url::to(['add-description'])?>" class="btn btn-info">Add Description</a>
 <ul class="list-group">
-    <?php use yii\helpers\Url;
+    <?php
 
     foreach ($model as $site):?>
     <a href="<?=Url::to(['update-description' , 'id' => $site['id']])?>"><li class="list-group-item"><?=$site['url']?></li></a>
