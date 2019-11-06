e2ea10ec2ffb372aa849f77d63dfb1fc071264b4
valearkot <valerakot97@gmail.com>
2019-11-06 18:55:26 +0200

    Update _create_form.php

--- src/views/site/_create_form.php
+++ src/views/site/_create_form.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\widgets\ActiveForm;
 
 ?>
@@ -9,12 +10,19 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'url')->textInput() ?>
+    <?= $form->field($model, 'url',[
+        'template' => '{label}<div class="input-group">
+        <span class="input-group-addon" id="basic-addon3">'.Url::base(true).'/</span>
+       {input}
+    </div>',
+    ])->textInput(['class'=>'form-control']) ?>
     <?php foreach ($language as $lang):?>
+    <?php if (count($language) != 1):?>
     <label>For <?=$lang?></label>
+    <?php endif;?>
         <?= $form->field($model, 'title['.$lang.']')->textInput()->label() ?>
         <?= $form->field($model, 'keywords['.$lang.']')->textInput()->label() ?>
-        <?= $form->field($model, 'description['.$lang.']')->textarea(['rows' => 6])->label() ?>
+        <?= $form->field($model, 'description['.$lang.']')->textInput()->label() ?>
     <?php endforeach;?>
 
 
