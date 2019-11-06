79064b103afe9c6c502ae18ec1471c7e10b95b37
valearkot <valerakot97@gmail.com>
2019-11-06 17:41:26 +0200

    Update _form.php

--- src/views/site/_form.php
+++ src/views/site/_form.php
@@ -9,15 +9,15 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'url')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'url')->textInput() ?>
     <?php foreach ($all_title as $key => $value):?>
-        <?= $form->field($model, 'title['.$key.']')->textarea(['rows' => 6,'value'=>$value])->label($key) ?>
+        <?= $form->field($model, 'title['.$key.']')->textInput('value'=>$value])->label() ?>
     <?php endforeach;?>
     <?php foreach ($all_keywords as $key => $value):?>
-        <?= $form->field($model, 'title['.$key.']')->textarea(['rows' => 6,'value'=>$value])->label($key) ?>
+        <?= $form->field($model, 'title['.$key.']')->textInput('value'=>$value])->label() ?>
     <?php endforeach;?>
     <?php foreach ($all_description as $key => $value):?>
-        <?= $form->field($model, 'description['.$key.']')->textarea(['rows' => 6,'value'=>$value])->label($key) ?>
+        <?= $form->field($model, 'description['.$key.']')->textarea(['rows' => 6,'value'=>$value])->label() ?>
     <?php endforeach;?>
 
 
